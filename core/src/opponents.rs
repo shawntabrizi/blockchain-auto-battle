9a528585a3c0559a7e54ce267ead91a456548f1e
Shawn Tabrizi <shawntabrizi@gmail.com>
2026-01-22 17:43:24 -0400

    refactor all units

--- core/src/opponents.rs
+++ core/src/opponents.rs
@@ -1,117 +1,79 @@
-use crate::types::{Ability, AbilityEffect, AbilityTarget, AbilityTrigger, BoardUnit, UnitCard};
+use crate::types::{BoardUnit, UnitCard};
+use crate::units::get_starter_templates;
 
-/// Create a unit with an optional ability
-fn create_unit(
-    card_id_counter: &mut u32,
-    template_id: &str,
-    name: &str,
-    attack: i32,
-    health: i32,
-    ability: Option<Ability>,
-) -> BoardUnit {
+/// Create a unit from a template
+fn create_unit_from_template(card_id_counter: &mut u32, template_id: &str) -> BoardUnit {
     *card_id_counter += 1;
-    let mut card = UnitCard::new(*card_id_counter, template_id, name, attack, health, 0, 0);
-    if let Some(a) = ability {
-        card = card.with_ability(a);
-    }
-    BoardUnit::from_card(card)
-}
 
-/// Get the ability for a specific opponent unit type
-fn get_opponent_ability(template_id: &str) -> Option<Ability> {
-    match template_id {
-        "orc_shaman" => Some(Ability {
-            trigger: AbilityTrigger::OnStart,
-            effect: AbilityEffect::ModifyStats {
-                health: 2,
-                attack: 0,
-                target: AbilityTarget::FrontAlly,
-            },
-            name: "Healing Totem".to_string(),
-            description: "Heal front ally for 2 at battle start".to_string(),
-        }),
-        "dragon_tyrant" => Some(Ability {
-            trigger: AbilityTrigger::OnStart,
-            effect: AbilityEffect::Damage {
-                amount: 3,
-                target: AbilityTarget::AllEnemies,
-            },
-            name: "Dragon Breath".to_string(),
-            description: "Deal 3 damage to all enemies at battle start".to_string(),
-        }),
-        _ => None,
+    // Get all starter templates (this includes all units now)
+    let templates = get_starter_templates();
+
+    // Find the template
+    let template = templates
+        .into_iter()
+        .find(|t| t.template_id == template_id)
+        .expect(&format!("Template '{}' not found", template_id));
+
+    // Create the unit card with the template data
+    let mut card = UnitCard::new(
+        *card_id_counter,
+        template.template_id,
+        &template.name,
+        template.attack,
+        template.health,
+        template.play_cost,
+        template.pitch_value,
+    );
+
+    // Add abilities if any
+    for ability in template.abilities {
+        card = card.with_ability(ability);
     }
+
+    BoardUnit::from_card(card)
 }
 
 /// Get the opponent board for a given round (1-10)
 pub fn get_opponent_for_round(round: i32, card_id_counter: &mut u32) -> Vec<BoardUnit> {
     let mut units = Vec::new();
-    let templates = match round {
-        1 => vec![("goblin_scout", "Goblin Scout", 1, 2)],
-        2 => vec![
-            ("goblin_scout", "Goblin Scout", 1, 2),
-            ("goblin_grunt", "Goblin Grunt", 2, 2),
-        ],
-        3 => vec![
-            ("orc_shaman", "Orc Shaman", 2, 3),
-            ("goblin_scout", "Goblin Scout", 1, 2),
-            ("goblin_grunt", "Goblin Grunt", 2, 2),
-        ],
-        4 => vec![
-            ("orc_warrior", "Orc Warrior", 3, 4),
-            ("orc_shaman", "Orc Shaman", 2, 3),
-            ("goblin_grunt", "Goblin Grunt", 2, 2),
-        ],
-        5 => vec![
-            ("troll_brute", "Troll Brute", 4, 5),
-            ("orc_warrior", "Orc Warrior", 3, 3),
-            ("orc_shaman", "Orc Shaman", 2, 3),
-        ],
+    let template_ids = match round {
+        1 => vec!["goblin_scout"],
+        2 => vec!["goblin_scout", "goblin_grunt"],
+        3 => vec!["orc_shaman", "goblin_scout", "goblin_grunt"],
+        4 => vec!["orc_warrior", "orc_shaman", "goblin_grunt"],
+        5 => vec!["troll_brute", "orc_warrior", "orc_shaman"],
         6 => vec![
-            ("troll_brute", "Troll Brute", 4, 6),
-            ("troll_warrior", "Troll Warrior", 3, 5),
-            ("orc_warrior", "Orc Warrior", 3, 3),
-            ("goblin_grunt", "Goblin Grunt", 2, 2),
+            "troll_brute",
+            "troll_warrior",
+            "orc_warrior",
+            "goblin_grunt",
         ],
-        7 => vec![
-            ("ogre_mauler", "Ogre Mauler", 5, 7),
-            ("troll_brute", "Troll Brute", 4, 5),
-            ("troll_warrior", "Troll Warrior", 3, 5),
-            ("orc_shaman", "Orc Shaman", 2, 3),
-        ],
+        7 => vec!["ogre_mauler", "troll_brute", "troll_warrior", "orc_shaman"],
         8 => vec![
-            ("ogre_mauler", "Ogre Mauler", 5, 8),
-            ("ogre_warrior", "Ogre Warrior", 4, 6),
-            ("troll_brute", "Troll Brute", 4, 5),
-            ("troll_warrior", "Troll Warrior", 3, 5),
+            "ogre_mauler",
+            "ogre_warrior",
+            "troll_brute",
+            "troll_warrior",
         ],
         9 => vec![
-            ("giant_crusher", "Giant Crusher", 6, 9),
-            ("ogre_mauler", "Ogre Mauler", 5, 7),
-            ("ogre_warrior", "Ogre Warrior", 4, 6),
-            ("troll_brute", "Troll Brute", 4, 5),
-            ("orc_shaman", "Orc Shaman", 2, 3),
+            "giant_crusher",
+            "ogre_mauler",
+            "ogre_warrior",
+            "troll_brute",
+            "orc_shaman",
         ],
         _ => vec![
             // Default to round 10 for any round beyond
-            ("dragon_tyrant", "Dragon Tyrant", 8, 12),
-            ("giant_crusher", "Giant Crusher", 6, 8),
-            ("ogre_mauler", "Ogre Mauler", 5, 7),
-            ("ogre_warrior", "Ogre Warrior", 4, 6),
-            ("troll_brute", "Troll Brute", 4, 5),
+            "dragon_tyrant",
+            "giant_crusher",
+            "ogre_mauler",
+            "ogre_warrior",
+            "troll_brute",
         ],
     };
 
-    for (template_id, name, attack, health) in templates {
-        let ability = get_opponent_ability(template_id);
-        units.push(create_unit(
-            card_id_counter,
-            template_id,
-            name,
-            attack,
-            health,
-            ability,
-        ));
+    for template_id in template_ids {
+        units.push(create_unit_from_template(card_id_counter, template_id));
     }
 
     units
