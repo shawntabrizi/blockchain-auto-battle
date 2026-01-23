66b02575fbef4af4f50b3504be074149be2e67bf
Shawn Tabrizi <shawntabrizi@gmail.com>
2026-01-23 00:40:36 -0400

    Add safety limits to the trigger queue to prevent execution overflows

--- core/src/limits.rs
+++ core/src/limits.rs
@@ -3,6 +3,7 @@
 pub const MAX_RECURSION_DEPTH: u32 = 50;
 pub const MAX_SPAWNS_PER_BATTLE: u32 = 100;
 pub const MAX_TRIGGERS_PER_PHASE: u32 = 200;
+pub const MAX_TRIGGER_DEPTH: u32 = 10;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Team {
@@ -23,6 +24,7 @@ impl Team {
 #[derive(Debug, Clone)]
 pub struct BattleLimits {
     pub recursion_depth: u32,
+    pub trigger_depth: u32,
     pub total_spawns: u32,
     pub phase_triggers: u32,
     pub current_executing_team: Option<Team>,
@@ -34,6 +36,7 @@ impl BattleLimits {
     pub fn new() -> Self {
         Self {
             recursion_depth: 0,
+            trigger_depth: 0,
             total_spawns: 0,
             phase_triggers: 0,
             current_executing_team: None,
@@ -96,5 +99,25 @@ impl BattleLimits {
             return Err(());
         }
         Ok(())
+    }
+
+    pub fn enter_trigger_depth(&mut self, team: Team) -> Result<(), ()> {
+        self.current_executing_team = Some(team);
+        self.trigger_depth += 1;
+        if self.trigger_depth > MAX_TRIGGER_DEPTH {
+            self.limit_exceeded_by = Some(team);
+            self.limit_exceeded_reason = Some(format!(
+                "Trigger depth exceeded (max {})",
+                MAX_TRIGGER_DEPTH
+            ));
+            return Err(());
+        }
+        Ok(())
+    }
+
+    pub fn exit_trigger_depth(&mut self) {
+        if self.trigger_depth > 0 {
+            self.trigger_depth -= 1;
+        }
     }
 }
