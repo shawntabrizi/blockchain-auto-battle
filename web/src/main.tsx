bc4fded10b9097ad581df8dfb5d5805010d22e8a
Shawn Tabrizi <shawntabrizi@gmail.com>
2026-01-26 01:08:46 -0400

    Refactor React entry point and update routing for multiplayer support

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import { SandboxPage } from './components/SandboxPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/sandbox" element={<SandboxPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
