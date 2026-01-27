5bf92d908328cf7910d3db7dff9bac5ea4b93668
Shawn Tabrizi <shawntabrizi@gmail.com>
2026-01-27 15:46:31 -0400

    Update blockchain target locations in chain_spec.json and scripts

--- blockchain/start_chain.sh
+++ blockchain/start_chain.sh
@@ -9,7 +9,7 @@ cargo build -p auto-battle-runtime --release --locked
 chain-spec-builder create -t development \
 --relay-chain paseo \
 --para-id 1000 \
---runtime ./target/release/wbuild/auto-battle-runtime/auto_battle_runtime.compact.compressed.wasm \
+--runtime ../target/release/wbuild/auto-battle-runtime/auto_battle_runtime.compact.compressed.wasm \
 named-preset development > chain_spec.json
 
 # Start the node
