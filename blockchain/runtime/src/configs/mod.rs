b2729cd3dd5fa7abbe81585973a17aa73ff6848f
Shawn Tabrizi <shawntabrizi@gmail.com>
2026-01-31 16:32:20 -0800

    fix build

--- blockchain/runtime/src/configs/mod.rs
+++ blockchain/runtime/src/configs/mod.rs
@@ -354,5 +354,6 @@ impl pallet_auto_battle::Config for Runtime {
     type MaxHandActions = ConstU32<10>;
     type MaxAbilities = ConstU32<5>;
     type MaxStringLen = ConstU32<32>;
+    type MaxConditions = ConstU32<5>;
     type MaxGhostsPerBracket = ConstU32<10>;
 }
